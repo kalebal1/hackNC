a1d227ec21aca4cda7525fd6e552ee39986b4d60
Kaleb Lyda <kalebal@live.unc.edu>
2020-10-17 14:56:18 -0400

    configure google-sign in objects

--- FitFriend/app/src/main/java/com/example/fitfriend/ui/login/LoginActivity.java
+++ FitFriend/app/src/main/java/com/example/fitfriend/ui/login/LoginActivity.java
@@ -30,6 +30,15 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
 
+    public void onStart(){
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null) {
+            // skip straight to main menu
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,6 +126,14 @@ public class LoginActivity extends AppCompatActivity {
                         passwordEditText.getText().toString());
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
