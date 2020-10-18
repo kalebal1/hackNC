0673e88f1da7b1adb5f9c6c4da5a8121b6c7ad5e
Kaleb Lyda <kalebal@live.unc.edu>
2020-10-17 22:38:09 -0400

    align buttons

---
+++ FitFriend/conversion.js
@@ -0,0 +1,10 @@
+//convert from imperial to metric on settings button click
+object.addEventListener("click", myScript);
+
+function convertToMetric(){
+    document.getElementById("imperial_unit").disabled;
+}
+
+function convertToImperial(){
+    document.getElementById("metric_unit").disabled;
+}
