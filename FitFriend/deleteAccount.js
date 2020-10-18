f23702844cadaa0d223860412e0f3df2bf7e55d1
Kaleb Lyda <kalebal@live.unc.edu>
2020-10-18 09:54:30 -0400

    fix home link

--- FitFriend/deleteAccount.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("delete-btn").onclick = function() {deleteAccount()};
-
-function deleteAccount(){
-    if(confirm("Are you sure? This action can't be undone.")){
-        //delete their account i guess
-    }
-}
