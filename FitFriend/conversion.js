23c4a507129797de0d347d5d17528d9d1583c026
Kaleb Lyda <kalebal@live.unc.edu>
2020-10-18 01:41:04 -0400

    leaderboard skeleton

--- FitFriend/conversion.js
+++ FitFriend/conversion.js
@@ -1,6 +1,4 @@
 //convert from imperial to metric on settings button click
-object.addEventListener("click", myScript);
-
 function convertToMetric(){
     document.getElementById("imperial_unit").disabled;
 }
---
+++ FitFriend/deleteAccount.js
@@ -0,0 +1,7 @@
+document.getElementById("delete-btn").onclick = function() {deleteAccount()};
+
+function deleteAccount(){
+    if(confirm("Are you sure? This action can't be undone.")){
+        //delete their account i guess
+    }
+}
